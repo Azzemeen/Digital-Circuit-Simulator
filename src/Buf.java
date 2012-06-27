1ab86fa59b6f91d6a17ab63f34a6cbb0f21bcfd5
Azzemeen <Azzemeen@gmx.de>
2012-06-27 15:53:57 +0300

    Update patch-11

--- src/Buf.java
+++ src/Buf.java
@@ -1,12 +1,21 @@
+package Stufe3;
 
 public class Buf extends Gatter {
 
 	public Buf(int delay) {
-		super(1, delay);
-		// TODO Auto-generated constructor stub
-	}
-	public void berechne() {
-		
+		super(delay);
+
 	}
 
+	public void berechne() {
+
+	}
+
+	public void berechne(int t) {
+		boolean ergebnis = true;
+		for (Signal s : eingangssignale) {
+			ergebnis = s.getValue();
+			new Event(ausgangssignal, t += delay, ergebnis);
+		}
+	}
 }
